076d76d84cac96cf6c024aae7ba9464bd278cac0
Mr.Damage <heath@cloudbreakyoga.com>
2017-04-27 15:49:02 -0700

    this is me trying to clean up the code
    code needed cleaning up before asking a question.

--- zhvimapSE.R
+++ zhvimapSE.R
@@ -1,4 +1,5 @@
 
+
 #clean up environment
 rm(list = ls())
 
@@ -23,24 +24,31 @@ urldata <- GET(url)
 zchomes <- read.csv("http://files.zillowstatic.com/research/public/County/County_Zhvi_Summary_AllHomes.csv", header = TRUE, sep = ",")
 county_df <- map_data("county")
 state_df <- map_data("state")
-buoyID <- c(41110,44096) #creating data frame to plot the locations of weather buoys in the ocean
-index <- c(2713480,2791621)
-lat <- c(34.141,37.023)
-lon <- c(-77.709,-75.810)
-buoyPassSE <- data.frame(buoyID, index, lat, lon)
 
+#converting factors
+buoyID.factor <- factor(c(41110,44096))
+index.factor <- factor(c(2713480,2791621))
+lat.factor <- factor(c(34.141,37.023))
+lon.factor <- factor(c(-77.709,-75.810))
 
-#data formatting
-zchomesCoast <- filter(zchomes, grepl('FL|SC|NC', zchomes$State)) #only interested in certain states
+buoyID <- as.numeric(as.character(buoyID.factor))
+index <- as.numeric(as.character(index.factor))
+lat <- as.numeric(as.character(lat.factor))
+lon <- as.numeric(as.character(lon.factor))
+
+buoyPassSE <- as.data.frame(buoyID, index, lat, lon, row.names = NULL)
+
+#data formatting so that I'm only focusing on certain states
+zchomesCoast <- filter(zchomes, grepl('FL|SC|NC', zchomes$State))
 countyCoast_df <- filter(county_df, grepl('florida|south carolina|north carolina', county_df$region))
 countyCoastSurf_df <- filter(countyCoast_df, grepl('brevard|onslow|duval|new hanover|volusia|st lucie|indian river', countyCoast_df$subregion))
 stateCoast_df <- filter(state_df, grepl('florida|south carolina|north carolina', state_df$region))
 colnames(countyCoast_df) <- c("long","lat","group", "order","state","county") #for ggplot
 zchomesCoast$county <- tolower(zchomesCoast$RegionName)
 
-#data merge of the county coastal information and the zillow home index information
+#data merge
 choropleth <- merge(countyCoast_df, zchomesCoast, by = c("county"))
-choropleth <- choropleth[order(choropleth$order), ] #I don't understand what this is doing, but it doesn't work without it
+choropleth <- choropleth[order(choropleth$order), ]
 
 #now for the plotting
 ggplot(choropleth, aes(long, lat, group=group), environment = environment())+
@@ -52,18 +60,14 @@ ggplot(choropleth, aes(long, lat, group=group), environment = environment())+
   scale_fill_gradient(low = '#ffff99', high = '#ff0000',
                       breaks=c(300000,800000,1200000),
                       label = comma)+
-  
+ 
+#this provides an overlay of certain counties 
   geom_polygon(data = countyCoastSurf_df, aes(long, lat,group=group), fill='#66ff66', colour= (alpha=1))+
   
-  #I can't get the following points to also show up on this map.
-  #geom_point(data = buoyPassSE, aes(x = -77.70899963, y = 34.14099884, fill = "red", alpha = 0.8,group = group), inherit.aes=FALSE,size = 5, shape = 21) +
-  
-  #geom_point(data = buoyPass, aes(x = -120.45, y = 34.27, fill = "red", alpha = 0.8), size = 5, shape = 21) +
-  #geom_point(data = sspotlocs, aes(x = -119.48, y = 34.38, fill = "red", alpha = 0.8), size = 5, shape = 21) +
-  #geom_point(data = buoyPass, aes(x = -120.97, y = 34.71, fill = "red", alpha = 0.8), size = 5, shape = 21) +
-  #geom_point(data = buoyPass, aes(x = -120.45, y = 34.27, fill = "red", alpha = 0.8), size = 5, shape = 21) +
-  #geom_point(data = buoyPass, aes(x = -120.45, y = 34.27, fill = "red", alpha = 0.8), size = 5, shape = 21) +
-  
+#I can't get the following points to also show up on this map.
+#without this line, the script works.
+  geom_point(data = buoyPassSE, aes(x = lon, y = lat, fill = diff, group=group), inherit.aes = FALSE, alpha = 0.8, size = 5, pch = 21) +
+
   theme_nothing()+ 
-  coord_map(xlim = c(-90, -70),ylim = c(25, 37)) #this zooms down on the SE
+  coord_map(xlim = c(-90, -70),ylim = c(25, 37))
 
